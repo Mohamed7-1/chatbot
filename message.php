dec0fe43957ebe26dcc0b3222bf16690167b8295
Mohamed7-1 <151441158+Mohamed7-1@users.noreply.github.com>
2024-09-02 13:53:13 +0100

    Update message.php for pdf

--- message.php
+++ message.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+require('fpdf/fpdf.php'); // Include FPDF library
 
 // Connecting to the database
-$conn = mysqli_connect("localhost", "root", "", "bot") or die("Database Error");
+$conn = mysqli_connect("localhost", "dolibarrmysql", "mohamed2017", "dolibarr") or die("Database Error");
+
 
 // Getting user message through AJAX
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
@@ -15,17 +17,29 @@ $run_query = mysqli_query($conn, $check_data) or die("Error");
 if (mysqli_num_rows($run_query) > 0) {
     // Fetching reply from the database according to the user query
     $fetch_data = mysqli_fetch_assoc($run_query);
-    // Storing reply in a variable which we'll send to AJAX
     $replay = $fetch_data['replies'];
-    echo $replay;
+
+    // Generate PDF
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', '', 12);
+    $pdf->MultiCell(0, 10, $replay); // Use MultiCell for wrapping text
+    $filename = 'reply.pdf';
+    $pdf->Output('F', $filename);
+
+    // Output PDF link
+    echo json_encode(["pdf" => $filename]);
 
     // Store chat history in session
     if (!isset($_SESSION['chat_history'])) {
         $_SESSION['chat_history'] = [];
     }
     $_SESSION['chat_history'][] = "User: $getMesg";
-    $_SESSION['chat_history'][] = "Bot: $replay";
+    $_SESSION['chat_history'][] = "Bot: Sent a PDF";
 } else {
-    echo "Sorry, can't be able to understand you!";
+    echo json_encode(["error" => "Sorry, can't be able to understand you!"]);
 }
 ?>
+
+
+
